a0170ba60229dfdf36feec365f6fd8adcd730fd0
HMixany <hmixany@gmail.com>
2024-02-20 18:50:25 +0200

    Initial commit

---
+++ src/entity/models.py
@@ -0,0 +1,19 @@
+from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import String, Date
+from sqlalchemy.orm import DeclarativeBase
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+class Todo(Base):
+    __tablename__ = "contacts"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    first_name: Mapped[str] = mapped_column(String(120), nullable=False)
+    last_name: Mapped[str] = mapped_column(String(120), nullable=False)
+    email: Mapped[str] = mapped_column(String(50), nullable=False)
+    phone: Mapped[str] = mapped_column(String(100), nullable=False)
+    birthday: Mapped[str] = mapped_column(Date)
+    description: Mapped[str] = mapped_column(String(250), nullable=True)
+
---
+++ src/schemas/contact.py
@@ -0,0 +1,5 @@
+class Config:
+    DB_URL = "postgresql+asyncpg://postgres:321456@localhost:5432/WHW11"
+
+
+config = Config
