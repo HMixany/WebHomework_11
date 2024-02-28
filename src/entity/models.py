a840911ebd0f72c55894a026de97f86c65220776
HMixany <hmixany@gmail.com>
2024-02-28 23:54:20 +0200

    second commit

--- src/entity/models.py
+++ src/entity/models.py
@@ -1,5 +1,7 @@
-from sqlalchemy.orm import Mapped, mapped_column
-from sqlalchemy import String, Date
+from datetime import date
+
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from sqlalchemy import String, Integer, Date, ForeignKey, DateTime, func
 from sqlalchemy.orm import DeclarativeBase
 
 
@@ -16,4 +18,20 @@ class Contact(Base):
     phone: Mapped[str] = mapped_column(String(100), nullable=False)
     birthday: Mapped[str] = mapped_column(Date)
     description: Mapped[str] = mapped_column(String(250), nullable=True)
+    created_at: Mapped[date] = mapped_column("created_at", DateTime, default=func.now(), nullable=True)
+    updated_at: Mapped[date] = mapped_column("updated_at", DateTime, default=func.now(), onupdate=func.now(),
+                                             nullable=True)
+    user_id: Mapped[int] = mapped_column(Integer, ForeignKey("users.id"), nullable=True)
+    user: Mapped["User"] = relationship("User", backref="todos", lazy="joined")
 
+
+class User(Base):
+    __tablename__ = "users"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    username: Mapped[str] = mapped_column(String(50))
+    email: Mapped[str] = mapped_column(String(150), nullable=False, unique=True)
+    password: Mapped[str] = mapped_column(String(255))
+    avatar: Mapped[str] = mapped_column(String(250), nullable=True)
+    refresh_token: Mapped[str] = mapped_column(String(255), nullable=True)
+    created_at: Mapped[date] = mapped_column("created_at", DateTime, default=func.now())
+    updated_at: Mapped[date] = mapped_column("updated_at", DateTime, default=func.now(), onupdate=func.now())
---
+++ src/repository/users.py
@@ -0,0 +1,30 @@
+from fastapi import Depends, HTTPException, status
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
+from libgravatar import Gravatar
+
+from src.entity.models import User
+from src.schemas.user import UserSchema
+from src.database.db import get_db
+
+
+async def get_user_by_email(email: str, db: AsyncSession = Depends(get_db)):
+    stmt = select(User).filter_by(email=email)
+    user = await db.execute(stmt)
+    return user.scalar_one_or_none()
+
+
+async def create_user(body: UserSchema, db: AsyncSession):
+    avatar = None
+    try:
+        g = Gravatar(body.email)
+        avatar = g.get_image()
+    except Exception as err:
+        print(err)
+
+    new_user = User(**body.model_dump(), avatar=avatar)
+    db.add(new_user)
+    await db.commit()
+    await db.refresh(new_user)
+    return new_user
